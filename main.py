3c4e3dbf343b74a0d26ec556072ff18f02afee57
Amjad-07 <146514436+Amjad-07@users.noreply.github.com>
2025-05-08 14:36:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello from Cloud Run CI/CD with Python!'
+    return "Hello from Cloud Run CI/CD!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
